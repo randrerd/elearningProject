39e20aafc4d34b7385503ed708cfe836f768de0e
randrerd <raulromand@gmail.com>
2020-04-09 00:55:00 -0300

    style mobile responsive landing page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
-  groupCells: 3
+  groupCells: true,
 });
 
 // element argument can be a selector string
@@ -11,3 +11,11 @@ var flkty = new Flickity( elem, {
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
+
+let btn = document.getElementById("navButton");
+let navLinks = document.getElementById("navLinks");
+
+btn.onclick = function () {
+  btn.classList.toggle("is-active");
+  navLinks.classList.toggle("mobile");
+};
